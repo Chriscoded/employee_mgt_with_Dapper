5e323adca5b053ce68dc8ac7c80e4f19380b69e2
Chriscoded <amaemechris@gmail.com>
2022-10-09 06:54:28 +0100

    09/1/22 commit

--- employee_mgt_with_Dapper/Controllers/EmployeeController.cs
+++ employee_mgt_with_Dapper/Controllers/EmployeeController.cs
@@ -3,43 +3,5 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace employee_mgt_with_Dapper.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmployeeController : ControllerBase
-    {
-        private IEmployeeRepository _employeeRepo;
-
-        public EmployeeController(IEmployeeRepository employeeRepo)
-        {
-            _employeeRepo = employeeRepo;
-        }
-
-        [HttpGet]
-        public async Task<string> Get()
-        {
-            var employees = await _employeeRepo.GetAllAsync();
-            return Newtonsoft.Json.JsonConvert.SerializeObject(employees);
-        }
-
-        [HttpPost]
-        public IActionResult Create(Employee employee)
-        {
-            _employeeRepo.InsertAsync(employee);
-            return Ok();
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, Employee employee)
-        {
-            _employeeRepo.UpdateAsync(employee);
-            return Ok();
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            _employeeRepo.DeleteAsync(id);
-            return Ok();
-        }
-    }
+   
 }
