6cf2c580f2e414335213f16f9a9b5ee0e5c75003
Chriscoded <amaemechris@gmail.com>
2022-10-09 06:45:45 +0100

    Add project files.

---
+++ employee_mgt_with_Dapper/Repositories/Employees/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace employee_mgt_with_Dapper
+{
+    public interface IEmployeeRepository : IGenericRepository<Employee>
+    {
+        Task<bool> MyCustomRepositoryMethodExampleAsync();
+    }
+}
